1cb3821e4a0f7378b6533f29db14e9cd82c77355
z4yx <z4yx@users.noreply.github.com>
2023-08-19 23:43:48 +0800

    more tests for CM

--- tests/standard/fido2v1/test_credmgmt.py
+++ tests/standard/fido2v1/test_credmgmt.py
@@ -1,9 +1,11 @@
 import pytest
 import time
 import random
+from fido2.cose import EdDSA, ES256
 from fido2.ctap import CtapError
 from fido2.ctap2 import CredentialManagement
 from fido2.ctap2.pin import ClientPin, PinProtocolV1
+from fido2.utils import hmac_sha256, sha256
 from tests.utils import *
 from binascii import hexlify
 
@@ -19,11 +21,12 @@ def PinToken(request, device):
     return device.client.client_pin.get_pin_token(pin)
 
 def _get_pin_token_with_CM_permission(device):
-    return device.client.client_pin.get_pin_token(PIN, ClientPin.PERMISSION.CREDENTIAL_MGMT)
+    return device.sendPP(PIN, ClientPin.PERMISSION.CREDENTIAL_MGMT)
 
 
 @pytest.fixture(scope = 'function')
 def MC_RK_Res(device, PinToken):
+    results = []
     req = FidoRequest()
     rp = {"id": "ssh:", "name": "Bate Goiko"}
     req = FidoRequest(
@@ -32,7 +35,9 @@ def MC_RK_Res(device, PinToken):
         rp=rp,
         options={"rk": True},
     )
-    device.sendMC(*req.toMC())
+    res = device.sendMC(*req.toMC())
+    setattr(res, "request", req)
+    results.append(res)
 
     req = FidoRequest()
     rp = {"id": "xakcop.com", "name": "John Doe"}
@@ -42,8 +47,10 @@ def MC_RK_Res(device, PinToken):
         rp=rp,
         options={"rk": True},
     )
-    device.sendMC(*req.toMC())
-
+    res = device.sendMC(*req.toMC())
+    setattr(res, "request", req)
+    results.append(res)
+    return results
 
 @pytest.fixture(scope = 'function')
 def CredMgmt(device, PinToken):
@@ -105,6 +112,18 @@ def assert_cred_response_has_all_fields(cred_res):
     ):
         assert i in cred_res
 
+def assert_all_fields_of_cred_is_correct(cred_res, mc_req, mc_res):
+    FID = CredentialManagement.RESULT
+    assert cred_res[FID.USER]['id'] == mc_req.user['id']
+    assert cred_res[FID.USER]['name'] == mc_req.user['name']
+    assert cred_res[FID.USER]['displayName'] == mc_req.user['displayName']
+    assert cred_res[FID.CREDENTIAL_ID]['id'] == mc_res.auth_data.credential_data.credential_id[:]
+    assert cred_res[FID.CREDENTIAL_ID]['type'] == 'public-key'
+    assert cred_res[FID.PUBLIC_KEY] == mc_res.auth_data.credential_data.public_key
+    assert cred_res[FID.CRED_PROTECT] == mc_req.extensions['credProtect'] if \
+        (mc_req.extensions and 'credProtect' in mc_req.extensions) else 1
+    if mc_res.large_blob_key:
+        assert cred_res[FID.LARGE_BLOB_KEY] == mc_res.large_blob_key
 
 class TestCredentialManagement(object):
     def test_get_info(self, info):
@@ -200,6 +219,14 @@ class TestCredentialManagement(object):
             auth = device.sendGA(*req.toGA())
         assert e.value.code == CtapError.ERR.NO_CREDENTIALS
 
+        req.allow_list = [{
+            "id": reg.auth_data.credential_data.credential_id[:],
+            "type": "public-key",
+        }]
+        with pytest.raises(CtapError) as e:
+            auth = device.sendGA(*req.toGA())
+        assert e.value.code == CtapError.ERR.NO_CREDENTIALS
+
     def test_add_delete(self, device, PinToken, CredMgmt):
         """ Delete a credential in the 'middle' and ensure other credentials are not affected. """
 
@@ -240,12 +267,14 @@ class TestCredentialManagement(object):
         assert len(res) == 2
 
     def test_interleaved_add_delete(self, device, PinToken, CredMgmt):
-        rp = {"id": "new_rp.com"}
+        RPs = [{"id": "new_rp1.com"}, {"id": "new_rp2.com"}, {"id": "new_rp3.com"}]
         reg = None
         regs = {}
-        for time_limit in range(1000):
+        for op_num in range(1000):
             r = random.randint(1, 100)
-            if r < 85 or len(regs) == 0:
+            thres = 50 if op_num < 20 else 80
+            if r < thres or len(regs) == 0:
+                rp = random.choice(RPs)
                 req = FidoRequest(
                     pin=PIN,
                     options={"rk": True},
@@ -278,19 +307,180 @@ class TestCredentialManagement(object):
 
         CredMgmt.pin_uv_token = _get_pin_token_with_CM_permission(device)
 
-        # Check they all enumerate
-        creds = CredMgmt.enumerate_creds(reg.auth_data.rp_id_hash)
-        for cred in creds:
-            cred_id = cred[7]["id"]
-            user_id = cred[6]["id"]
-            print("CHECK: ", hexlify(cred_id))
-            assert cred_id in regs
-            assert user_id == regs[cred_id]
-            del regs[cred_id]
+        for rp in RPs:
+            rp_id_hash = sha256(rp['id'].encode('ascii'))
+            # Check them all
+            creds = CredMgmt.enumerate_creds(rp_id_hash)
+            for cred in creds:
+                cred_id = cred[7]["id"]
+                user_id = cred[6]["id"]
+                print("CHECK: ", hexlify(cred_id))
+                assert cred_id in regs
+                assert user_id == regs[cred_id]
+                del regs[cred_id]
 
         for cred_id in regs.keys():
-            print("NOT RETURN: ", hexlify(cred_id))
+            print("ERR! NOT RETURN: ", hexlify(cred_id))
         assert len(regs) == 0
+
+    def test_delete_with_rpid(self, device, PinToken, CredMgmt):
+        # create a new RK
+        rp = {"id": "example_3.com", "name": "John Doe 2"}
+        req = FidoRequest(
+            pin=PIN,
+            options={"rk": True},
+            rp=rp,
+        )
+        reg = device.sendMC(*req.toMC())
+
+        CredMgmt.pin_uv_token = device.sendPP(PIN, 
+            permissions=ClientPin.PERMISSION.CREDENTIAL_MGMT, 
+            permissions_rpid=rp['id'])
+
+        # get the ID from enumeration
+        creds = CredMgmt.enumerate_creds(reg.auth_data.rp_id_hash)
+        for cred in creds:
+            if cred[7]["id"] == reg.auth_data.credential_data.credential_id:
+                break
+
+        # delete it
+        cred = {"id": cred[7]["id"], "type": "public-key"}
+        CredMgmt.delete_cred(cred)
+
+        # make sure it doesn't work
+        req = FidoRequest(rp=rp)
+        with pytest.raises(CtapError) as e:
+            auth = device.sendGA(*req.toGA())
+        assert e.value.code == CtapError.ERR.NO_CREDENTIALS
+
+        # delete it again
+        with pytest.raises(CtapError) as e:
+            CredMgmt.delete_cred(cred)
+        assert e.value.code == CtapError.ERR.NO_CREDENTIALS
+
+    def test_missing_param(self, device, PinToken):
+        for cmd in range(1, 8):
+
+            if cmd == CredentialManagement.CMD.ENUMERATE_CREDS_BEGIN or \
+                cmd == CredentialManagement.CMD.DELETE_CREDENTIAL or \
+                cmd == CredentialManagement.CMD.UPDATE_USER_INFO:
+                # Missing subCommandParams
+                with pytest.raises(CtapError) as e:
+                    device.ctap2.credential_mgmt(cmd, pin_uv_protocol=1, pin_uv_param=32*b'U')
+                assert e.value.code == CtapError.ERR.MISSING_PARAMETER
+
+            if cmd != CredentialManagement.CMD.ENUMERATE_RPS_NEXT and \
+                cmd != CredentialManagement.CMD.ENUMERATE_CREDS_NEXT:
+                with pytest.raises(CtapError) as e:
+                    device.ctap2.credential_mgmt(cmd)
+                assert e.value.code == CtapError.ERR.PUAT_REQUIRED
+
+                # Missing pinUvAuthProtocol
+                with pytest.raises(CtapError) as e:
+                    device.ctap2.credential_mgmt(cmd, sub_cmd_params={}, pin_uv_param=32*b'U')
+                assert e.value.code == CtapError.ERR.MISSING_PARAMETER
+
+                with pytest.raises(CtapError) as e:
+                    device.ctap2.credential_mgmt(cmd, sub_cmd_params={}, pin_uv_protocol=-1, pin_uv_param=32*b'U')
+                assert e.value.code == CtapError.ERR.INVALID_PARAMETER
+
+    @pytest.mark.parametrize(
+        "perm", [None, ClientPin.PERMISSION.LARGE_BLOB_WRITE, ClientPin.PERMISSION.CREDENTIAL_MGMT]
+    )
+    @pytest.mark.parametrize(
+        "withRpID", [True, False]
+    )
+    def test_permission_check(self, perm, withRpID, device, MC_RK_Res, PinToken, CredMgmt):
+        rpid = "example.com" if withRpID else None
+        CredMgmt.pin_uv_token = device.sendPP(PIN, permissions=perm, permissions_rpid=rpid)
+
+        # delete_cred must be the last one
+        for method in (CredMgmt.get_metadata, CredMgmt.enumerate_rps, CredMgmt.enumerate_creds_begin, CredMgmt.update_user_info, CredMgmt.delete_cred):
+            kwargs = {}
+            if method == CredMgmt.enumerate_creds_begin:
+                kwargs['rp_id_hash'] = MC_RK_Res[0].auth_data.rp_id_hash
+            elif method == CredMgmt.delete_cred or method == CredMgmt.update_user_info:
+                kwargs['cred_id'] = {"id": MC_RK_Res[0].auth_data.credential_data.credential_id, "type": "public-key"}
+                if method == CredMgmt.update_user_info:
+                    kwargs['user_info'] = {"id": MC_RK_Res[0].request.user['id']}
+            if perm == ClientPin.PERMISSION.CREDENTIAL_MGMT and not withRpID:
+                method(**kwargs)
+            else:
+                with pytest.raises(CtapError) as e:
+                    method(**kwargs)
+                assert e.value.code == CtapError.ERR.PIN_AUTH_INVALID
+
+    @pytest.mark.parametrize(
+        "withRpID", [True, False]
+    )
+    def test_update_user_info(self, withRpID, device, MC_RK_Res, PinToken, CredMgmt):
+        rpid = MC_RK_Res[0].request.rp['id'] if withRpID else None
+        CredMgmt.pin_uv_token = device.sendPP(PIN, permissions=ClientPin.PERMISSION.CREDENTIAL_MGMT, permissions_rpid=rpid)
+
+        cred = {"id": MC_RK_Res[0].auth_data.credential_data.credential_id, "type": "public-key"}
+        user_info = generate_user()
+        del user_info['icon']
+        user_info['id'] = MC_RK_Res[0].request.user['id']
+        CredMgmt.update_user_info(cred, user_info)
+
+        ret_creds = CredMgmt.enumerate_creds(MC_RK_Res[0].auth_data.rp_id_hash)
+        assert len(ret_creds) == 1
+        assert ret_creds[0][6] == user_info
+
+        user_info['id'] = b'invalid uid'
+        with pytest.raises(CtapError) as e:
+            CredMgmt.update_user_info(cred, user_info)
+        assert e.value.code == CtapError.ERR.INVALID_PARAMETER
+
+        # delete it
+        CredMgmt.delete_cred(cred)
+
+        with pytest.raises(CtapError) as e:
+            CredMgmt.update_user_info(cred, user_info)
+        assert e.value.code == CtapError.ERR.NO_CREDENTIALS
+
+    def test_eddsa_enumerate(self, device, PinToken, MC_RK_Res, CredMgmt):
+        rpId = "EdDSA.com"
+        mc_req = FidoRequest(
+            pin=PIN,
+            options={"rk": True},
+            rp={"id": rpId},
+            key_params=[{"type": "public-key", "alg": EdDSA.ALGORITHM}]
+        )
+        try:
+            mc_res = device.sendMC(*mc_req.toMC())
+        except CtapError as e:
+            if e.code == CtapError.ERR.UNSUPPORTED_ALGORITHM:
+                print("ed25519 is not supported.  Skip this test.")
+                return
+
+        CredMgmt.pin_uv_token = device.sendPP(PIN, permissions=ClientPin.PERMISSION.CREDENTIAL_MGMT)
+        res = CredMgmt.enumerate_creds(sha256(rpId.encode('ascii')))
+        assert len(res) == 1
+        assert_all_fields_of_cred_is_correct(res[0], mc_req, mc_res)
+
+    def test_fields_of_cred_enumerate(self, device, info, PinToken, CredMgmt):
+        for credProtectVal in (None, 1, 2, 3):
+            for withLB in (False, True):
+                extensions = {}
+                if (credProtectVal is not None) and ('credProtect' in info.extensions):
+                    extensions['credProtect'] = credProtectVal
+                if withLB and ('largeBlobKey' in info.extensions):
+                    extensions['largeBlobKey'] = True
+                mc_req = FidoRequest(
+                    pin=PIN,
+                    options={"rk": True},
+                    extensions=extensions
+                )
+                mc_res = device.sendMC(*mc_req.toMC())
+                CredMgmt.pin_uv_token = device.sendPP(PIN, permissions=ClientPin.PERMISSION.CREDENTIAL_MGMT)
+                creds = CredMgmt.enumerate_creds(sha256(mc_req.rp['id'].encode('ascii')))
+                for cred in creds:
+                    if cred[7]['id'] == mc_res.auth_data.credential_data.credential_id[:]:
+                        assert_all_fields_of_cred_is_correct(cred, mc_req, mc_res)
+                        break
+                else:
+                    raise ValueError("Credential not in response")
 
 
     def test_multiple_creds_per_multiple_rps(
